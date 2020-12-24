4dab6816cca4592dccf6a4c08310fe16482a722a
Fedor Odintsov <fadmin@list.ru>
2020-12-24 07:24:15 +0300

    feat: add keyboard

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import './assets/styles/fontawesome/css/all.css';
 import './assets/images/rs_logo.png';
 import './assets/images/global-flag.png';
 import './eventlisteners';
+import './keyboard';
 
 window.onload = () => {
   const covid = new Dispatch();
