376739bd884ae550e9370167df879efab375880f
Katy Rog <katkopikat@gmail.com>
2020-12-24 14:09:40 +0300

    feat: add favicon

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import './assets/styles/loader.css';
 import './assets/styles/fontawesome/css/all.css';
 import './assets/images/rs_logo.png';
 import './assets/images/global-flag.png';
+import './assets/images/favicon.png';
 import './eventlisteners';
 import './keyboard';
 
+alert('Уважаемый проверяющий, подожди, плиз, хотя бы денёк: мой напарник борется с API и графиком!')
 window.onload = () => {
   const covid = new Dispatch();
   covid.start();
